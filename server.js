ef1e7a4fb527baa5c6346e52680e9dee0b574f44
Dev Sharma <100906703+Devsharma04@users.noreply.github.com>
2025-03-12 13:15:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://jobdekhoserver.onrender.com",
+    origin: "https://jobdekho-dusky.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
